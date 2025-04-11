bfd925b5188b415bc630902d1e82a0f8e1c9055d
kyphongg1412 <phongngo14122006@gmail.com>
2025-04-11 08:19:21 +0000

    💾 Hoàn thành Timestamp Microservice project

--- myApp.js
+++ myApp.js
@@ -1,13 +1,37 @@
-let express = require('express');
-let app = express();
-require('dotenv').config();
- app.get("/api", (req, res) => {
-  const thoigian = new Date();
-  res.json({
-    "unix":thoigian.getTime(),
-    "utc": thoigian.toUTCString()
-  });
- });
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date";
+
+app.get("/api/:date", (req, res) => {
+  let dateVao = req.params.date;
+  let date;
+
+  if (!isNaN(Number(dateVao))) {
+    date = new Date(Number(dateVao));
+  } else {
+    date = new Date(dateVao);
+  }
+
+  if (isInvalidDate(date)) {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
+
+app.get("/api", (req, res) => {
+  let gio = new Date();
+  res.json({ unix: gio.getTime(), utc: gio.toUTCString() });
+});
 
 
 
@@ -26,4 +50,6 @@ require('dotenv').config();
 
 
 
- module.exports = app;
+
+
+module.exports = app;
